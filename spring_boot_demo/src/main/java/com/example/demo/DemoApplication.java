2fa9f32b48f296ab865987e9bbed271e817889ca
waynebfhu(胡伟) <waynebfhu@tencent.com>
2021-08-12 17:27:58 +0800

    add spring web demo

---
+++ spring_boot_demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
+
+@RestController
+class GreetingController {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String mingzi) {
+		return new Greeting(counter.incrementAndGet(), String.format(template,mingzi));
+	}
+}
